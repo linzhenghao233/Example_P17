978f4de259d3de249384c27dabb0bbc316930395
linzhenghao233 <1392872316@qq.com>
2023-11-03 21:31:00 +0800

    1103

--- example_p17.cpp
+++ example_p17.cpp
@@ -27,3 +27,46 @@ int insert(sequenlist* L, datatype x, int i) {
 	}
 	return (1);
 }
+
+/* 删除第i个元素，元素从0开始计数 */
+int dellist(sequenlist* L, int i) {
+	if ((i < 0) || (i > L->last)) {
+		printf("error, please input the right i");
+
+		return 0;
+	}
+	else {
+		for (; i < L->last; i++)
+			L->data[i] = L->data[i + 1];
+		L->last = L->last - 1;
+
+		return (1);
+	}
+}
+
+/* 建立顺序表，其元素为单个字符 */
+void creatlist(sequenlist* L) {
+	int n, i;
+	char tmp;
+
+	printf("请输入数据的个数：\n");
+	scanf_s("%d", &n);
+	for (i = 0; i < n; i++) {
+		printf("data[%d] = ", i);
+		fflush(stdin);
+		scanf_s("%c", &tmp);
+		L->data[i] = tmp;
+	}
+	L->last = n - 1;
+	printf("\n");
+}
+
+/* 打印顺序表 */
+void printout(sequenlist* L) {
+	int i;
+
+	for (i = 0; i <= L->last; i++) {
+		printf("data[%d] = ", i);
+		printf("%c\n", L->data[i]);
+	}
+}
